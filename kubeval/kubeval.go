8d548956fee6e349be2359a917c1f1dd516884af
Andrey Zhelnin <andjelx@users.noreply.github.com>
2018-09-27 15:38:53 +0200

    Update kubeval.go

--- kubeval/kubeval.go
+++ kubeval/kubeval.go
@@ -2,6 +2,7 @@ package kubeval
 
 import (
 	"bytes"
+	"gzip"
 	"errors"
 	"fmt"
 	"runtime"
@@ -11,7 +12,6 @@ import (
 	"github.com/spf13/viper"
 	"github.com/xeipuuv/gojsonschema"
 	"gopkg.in/yaml.v2"
-	"github.com/garethr/kubeval/utils"
 )
 
 // Version represents the version of Kubernetes
@@ -53,6 +53,42 @@ type ValidationResult struct {
 	FileName string
 	Kind     string
 	Errors   []gojsonschema.ResultError
+}
+
+// Unpack gzipped file inplace
+func unpackGzipFile(gzFilePath, dstFilePath string) (int64, error) {
+    gzFile, err := os.Open(gzFilePath)
+    if err != nil {
+        return 0, fmt.Errorf("Failed to open file %s for unpack: %s", gzFilePath, err)
+    }
+    dstFile, err := os.OpenFile(dstFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+    if err != nil {
+        return 0, fmt.Errorf("Failed to create destination file %s for unpack: %s", dstFilePath, err)
+    }
+
+    ioReader, ioWriter := io.Pipe()
+
+    go func() { // goroutine leak is possible here
+        gzReader, _ := gzip.NewReader(gzFile)
+        // it is important to close the writer or reading from the other end of the
+        // pipe or io.copy() will never finish
+        defer func(){
+            gzFile.Close()
+            gzReader.Close()
+            ioWriter.Close()
+        }()
+
+        io.Copy(ioWriter, gzReader)
+    }()
+
+    written, err := io.Copy(dstFile, ioReader)
+    if err != nil {
+        return 0, err // goroutine leak is possible here
+    }
+    ioReader.Close()
+    dstFile.Close()
+
+    return written, nil
 }
 
 // detectLineBreak returns the relevant platform specific line ending
